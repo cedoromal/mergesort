bada119e4e606ce89134a3652e6f155d925d7b18
CEDoromal <30488557+CEDoromal@users.noreply.github.com>
2024-01-30 15:27:57 +0800

    adds reverse sort and custom sort

--- mergesort.go
+++ mergesort.go
@@ -1,29 +1,47 @@
 package mergesort
 
-import "cmp"
+type Number interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64
+}
 
-func Sort[T cmp.Ordered](arr []T) []T {
+func Sort[T Number](arr []T) []T {
+	return SortCustom(arr, func(a, b T) T { return a - b })
+}
+
+func SortReversed[T Number](arr []T) []T {
+	return SortCustom(arr, func(a, b T) T { return b - a })
+}
+
+func SortCustom[T Number](arr []T, comp func(a, b T) T) []T {
 	if len(arr) <= 1 {
 		return arr
 	}
 
-	arr1 := Sort(arr[:len(arr)/2])
-	arr2 := Sort(arr[len(arr)/2:])
+	arr1 := SortCustom(arr[:len(arr)/2], comp)
+	arr2 := SortCustom(arr[len(arr)/2:], comp)
 
-	return merge(arr1, arr2)
+	return merge(arr1, arr2, comp)
 }
 
-func merge[T cmp.Ordered](arr1 []T, arr2 []T) []T {
+func merge[T Number](arr1 []T, arr2 []T, comp func(a, b T) T) []T {
 	result := make([]T, len(arr1)+len(arr2))
 
 	i := 0
 	j := 0
 
 	for i < len(arr1) && j < len(arr2) {
-		if arr1[i] <= arr2[j] {
+		switch c := comp(arr1[i], arr2[j]); {
+		case c == 0:
 			result[i+j] = arr1[i]
 			i++
-		} else {
+			result[i+j] = arr2[j]
+			j++
+		case c < 0:
+			result[i+j] = arr1[i]
+			i++
+		case c > 0:
 			result[i+j] = arr2[j]
 			j++
 		}
--- mergesort_test.go
+++ mergesort_test.go
@@ -2,13 +2,12 @@ package mergesort_test
 
 import (
 	"reflect"
-	"sort"
 	"testing"
 
 	"modules.cedoromal.com/mergesort"
 )
 
-func TestMergeSort(t *testing.T) {
+func TestSort(t *testing.T) {
 	tests := []struct {
 		name     string
 		input    []int
@@ -43,11 +42,49 @@ func TestMergeSort(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := mergesort.Sort(tt.input)
-			if !sort.IntsAreSorted(actual) {
-				t.Errorf("Expected sorted slice, got %v", actual)
+			if actual := mergesort.Sort(tt.input); !reflect.DeepEqual(actual, tt.expected) {
+				t.Errorf("Expected %v, got %v", tt.expected, actual)
 			}
-			if !reflect.DeepEqual(actual, tt.expected) {
+		})
+	}
+}
+
+func TestSortReversed(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    []int
+		expected []int
+	}{
+		{
+			name:     "empty slice",
+			input:    []int{},
+			expected: []int{},
+		},
+		{
+			name:     "single element slice",
+			input:    []int{1},
+			expected: []int{1},
+		},
+		{
+			name:     "sorted slice",
+			input:    []int{1, 2, 3},
+			expected: []int{3, 2, 1},
+		},
+		{
+			name:     "reversed slice",
+			input:    []int{3, 2, 1},
+			expected: []int{3, 2, 1},
+		},
+		{
+			name:     "random slice",
+			input:    []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5},
+			expected: []int{9, 6, 5, 5, 5, 4, 3, 3, 2, 1, 1},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if actual := mergesort.SortReversed(tt.input); !reflect.DeepEqual(actual, tt.expected) {
 				t.Errorf("Expected %v, got %v", tt.expected, actual)
 			}
 		})
